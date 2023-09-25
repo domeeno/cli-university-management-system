2c079b9cc82c5ff6098232009bbfa47196a20269
domeeno <dominicflocea.dev@gmail.com>
2023-09-25 11:58:16 +0300

    first commit + car class example

---
+++ src/main/java/org/example/labone/Main.java
@@ -0,0 +1,8 @@
+package org.example.labone;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Sportage", "Kia", 20345);
+        System.out.println(car.getBrand());
+    }
+}
