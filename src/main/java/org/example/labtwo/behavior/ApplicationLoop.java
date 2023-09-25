7d91cd9cd5a7acc65ddfb29b68605e20a5921071
domeeno <dominicflocea.dev@gmail.com>
2023-09-25 15:02:17 +0300

    add simple example with oneline commands (argument passing)

--- src/main/java/org/example/labtwo/behavior/ApplicationLoop.java
+++ src/main/java/org/example/labtwo/behavior/ApplicationLoop.java
@@ -11,54 +11,68 @@ public class ApplicationLoop {
 
     private Scanner scanner;
     private University university;
+    private String command;
 
     public ApplicationLoop() {
         this.scanner = new Scanner(System.in);
         this.university = new University();
+        this.command = "";
     }
 
-
     public void run() {
-        String command = "djsoadsa";
+        while (!this.command.equals("q") ) {
+            this.command = takeUserInput();
 
-        while (!Objects.equals(command, "q")) {
-            command = this.takeUserInput();
-            String[] commandList = command.split("/");
+            String[] commandsList = this.command.split("/");
 
-            switch (commandList[0]) {
+            switch (commandsList[0]) {
                 case "f":
-                    this.addFaculty();
+                    handleFacultyCreate(commandsList);
                     break;
                 case "pf":
-                    this.printFaculties();
+                    printFaculties();
+                    break;
                 default:
                     System.out.println("Invalid command");
             }
         }
-
-        System.out.println("Exiting program");
-
         scanner.close();
     }
 
     private String takeUserInput() {
-        System.out.print("prompt> ");
-        return this.scanner.nextLine();
+
+        System.out.print("write command> ");
+        return scanner.nextLine();
     }
 
-    private void printFaculties() {
-        System.out.println(this.university);
+    private void handleFacultyCreate(String[] commands) {
+        if(commands.length == 4) {
+            addFaculty(commands);
+        } else {
+            addFaculty();
+        }
     }
 
     private void addFaculty() {
-        System.out.println("name: ");
-        String nameOfFaculty = scanner.nextLine();
-        System.out.println("abbrev: ");
-        String abbrevOfFaculty = scanner.nextLine();
-        System.out.println("field: ");
-        String fieldOfFaculty = scanner.nextLine();
+        // add faculty
+        System.out.println("faculty name:");
+        String facultyName = scanner.nextLine();
+        System.out.println("faculty abbrev:");
+        String facultyAbbrev = scanner.nextLine();
+        System.out.println("faculty field:");
+        String facultyField = scanner.nextLine();
 
-        Faculty faculty = new Faculty(nameOfFaculty, abbrevOfFaculty, fieldOfFaculty);
+        Faculty faculty = new Faculty(facultyName, facultyAbbrev, facultyField);
         this.university.addFaculty(faculty);
+    }
+
+    private void addFaculty(String[] arguments) {
+
+        Faculty faculty = new Faculty(arguments[1], arguments[2], arguments[3]);
+        this.university.addFaculty(faculty);
+    }
+
+    private void printFaculties() {
+        System.out.println(university);
     }
 }
